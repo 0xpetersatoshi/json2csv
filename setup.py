00aa34a7fc592ba5843d289239cbe5052e6e7cd2
pbegle <peter.begle@bytecode.io>
2021-04-23 00:02:25 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as fh:
+    long_description = fh.read()
+
+setup(
+    name='json2csv',
+    version='0.0.1',
+    author='Peter Begle',
+    author_email='peterbegle@gmail.com',
+    descrption='JSON to CSV CLI tool.',
+    long_description=long_description,
+    license='MIT',
+    packages=find_packages()
+)
